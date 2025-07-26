6bdc7883e00d2091037aa78cc59504115287f032
darmuh <darmuh8@gmail.com>
2025-07-26 00:16:53 -0700

    - Added ParsedInfo class in place of dictionary to allow for binding the same action name to multiple keys - Consolidated config parsing to ParseConfigItem method - Updated config parsing method to better handle unexpected formatting or invalid input - Updated valid values validation to not be case sensitive in AddBind - Removed some unused usings - Added check in parsing the config items for duplicate action/key binds - Added a slightly modified InitBindingsValidation from Permamiss' PR

--- AddControls/src/AddControls/BindManager.cs
+++ AddControls/src/AddControls/BindManager.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
 using static AddControls.Misc;
 using static AddControls.Plugin;
+using static UnityEngine.Rendering.DebugUI;
 
 namespace AddControls
 {
+    public class ParsedInfo(string actionName, string keyPath)
+    {
+        public string ActionName { get; set; } = actionName;
+        public string KeyPath { get; set; } = keyPath;
+    }
+
     public class BindManager
     {
-        internal static Dictionary<string, string> customBindings = [];
-        internal static Dictionary<string, string> removeList = [];
+        internal static List<ParsedInfo> customBindings = [];
+        internal static List<ParsedInfo> removeList = [];
 
         public static void Start()
         {
@@ -26,63 +32,86 @@ namespace AddControls
             InitCustomBindings();
             if (customBindings.Count > 0)
             {
-                customBindings.Do(c => AddBind(c.Key, c.Value));
+                customBindings.Do(c => AddBind(c.ActionName, c.KeyPath));
             }
         }
 
         public static void RemoveBindings()
         {
             InitBindsToRemove();
-
             if (removeList.Count > 0)
             {
-                removeList.Do(r => RemoveBind(r.Key, r.Value));
+                removeList.Do(r => RemoveBind(r.ActionName, r.KeyPath));
             }
+        }
+
+        private static bool ParseConfigItem(ref List<ParsedInfo> customList, string rawValue)
+        {
+            customList = [];
+
+            if (string.IsNullOrEmpty(rawValue))
+                return false;
+            else
+            {
+                if (string.IsNullOrEmpty(rawValue))
+                    return false;
+
+                foreach(var item in rawValue.Split(';', System.StringSplitOptions.RemoveEmptyEntries))
+                {
+                    var trimmed = item.Trim();
+
+                    if (string.IsNullOrEmpty(trimmed))
+                        continue;
+
+                    string[] parts = trimmed.Split(':', 2);
+                    
+                    if (parts.Length != 2)
+                        continue;
+
+                    var action = parts[0].Trim();
+                    var keypath = parts[1].Trim();
+
+                    if (customList.Any(bind => bind.ActionName == action && bind.KeyPath == keypath))
+                        continue;
+
+                    ParsedInfo parsedInfo = new(action, keypath);
+                    customList.Add(parsedInfo);
+                }
+            }
+
+            return customList.Count > 0;
         }
 
         private static void InitCustomBindings()
         {
-            customBindings = [];
-
-            if (string.IsNullOrEmpty(bindsAdded.Value))
+            if(!ParseConfigItem(ref customBindings, bindsAdded.Value))
                 Log.LogDebug("No custom binds to add!");
             else
-            {
-                if (bindsAdded.Value.EndsWith(';'))
-                    bindsAdded.Value = bindsAdded.Value.TrimEnd(';');
-
-                customBindings = bindsAdded.Value
-                    .Split(';')
-                   .Select(item => item.Trim())
-                   .Select(item => item.Split(':'))
-                   .ToDictionary(pair => pair[0].Trim(), pair => pair[1].Trim());
-            }
-
-            Log.LogDebug("Custom Bindings list created!");
+                Log.LogDebug("Custom Bindings list created!");
         }
 
         private static void InitBindsToRemove()
         {
             removeList = [];
 
-            if (string.IsNullOrEmpty(bindsRemoved.Value))
-                Log.LogDebug("No custom binds to add!");
+            if (!ParseConfigItem(ref removeList, bindsRemoved.Value))
+                Log.LogDebug("No binds to remove!");
             else
-            {
-                if (bindsRemoved.Value.EndsWith(';'))
-                    bindsRemoved.Value = bindsRemoved.Value.TrimEnd(';');
-
-                removeList = bindsRemoved.Value
-                    .Split(';')
-                   .Select(item => item.Trim())
-                   .Select(item => item.Split(':'))
-                   .ToDictionary(pair => pair[0].Trim(), pair => pair[1].Trim());
-            }
-
-            Log.LogDebug("removeList created!");
+                Log.LogDebug("removeList created!");
         }
 
-        //method used to actually update inputaction after validations
+        //slightly modified from PR https://github.com/Permamiss/AddControls/tree/master
+        public static void InitBindingsValidation()
+        {
+            if (customBindings.Count == 0 || removeList.Count == 0)
+                Log.LogDebug("No custom binds or removed binds to compare against!");
+            else
+                removeList.DoIf(bind => customBindings.Any(v => v.ActionName == bind.ActionName && v.KeyPath == bind.KeyPath), bind => customBindings.Remove(bind));
+
+            Log.LogDebug("Custom Bindings list & removeList validated!");
+        }
+
+        //method used to actually update input action after validations
         private static void AddActionBind(InputAction inputAction, string Name, string Value)
         {
             if (inputAction == null)
@@ -95,6 +124,7 @@ namespace AddControls
             Log.LogMessage($"Added binding for {Name} with {Value}");
         }
 
+        //method used to actually clear an existing binding
         private static void RemoveActionBind(InputAction inputAction, string Name, string Value)
         {
             if (inputAction == null)
@@ -112,7 +142,6 @@ namespace AddControls
         {
             InputAction inputAction = InputSystem.actions.FindAction(actionName);
 
-            //if (!InputSystem.actions.Any(a => a.name == actionName))
             if (inputAction == null)
             {
                 Log.LogWarning($"Unable to remove bind {actionName} to {value} (INVALID ACTION)");
@@ -133,20 +162,18 @@ namespace AddControls
         {
             InputAction inputAction = InputSystem.actions.FindAction(actionName);
 
-            //if (!InputSystem.actions.Any(a => a.name == actionName))
             if(inputAction == null)
             {
                 Log.LogWarning($"Unable to bind {actionName} to {value} (INVALID ACTION)");
                 return;
             }
 
-            if (!ValidValues.Contains(value))
+            if (!ValidValues.Any(v => v.Equals(value, System.StringComparison.InvariantCultureIgnoreCase)))
             {
                 Log.LogWarning($"Unable to bind {actionName} to {value} (INVALID VALUE)");
                 return;
             }
 
-            //InputAction inputAction = InputSystem.actions.FindAction(actionName);
             AddActionBind(inputAction, actionName, value);
         }
     }
